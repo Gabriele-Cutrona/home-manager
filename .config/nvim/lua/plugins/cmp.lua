9571290413b6e8cdb24e310be7f9af9fd8b5e7d1
Gabriele Cutrona <106313541+Gabriele-Cutrona@users.noreply.github.com>
2024-07-01 16:12:21 +0200

    comment out supermaven

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -49,7 +49,7 @@ return {
                { name = "nvim_lsp" },
                { name = "luasnip" },
                { name = "path" },
-               { name = "supermaven" },
+               -- { name = "supermaven" },
             },
             window = {
                completion = cmp.config.window.bordered(),
@@ -58,10 +58,10 @@ return {
          })
       end,
    },
-   {
-      "supermaven-inc/supermaven-nvim",
-      config = function()
-         require("supermaven-nvim").setup({})
-      end,
-   },
+   -- {
+   --    "supermaven-inc/supermaven-nvim",
+   --    config = function()
+   --       require("supermaven-nvim").setup({})
+   --    end,
+   -- },
 }
